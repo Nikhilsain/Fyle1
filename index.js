2b867348c463fe6a9de26d1487f52ff9e4fd17df
Nikhil Sain <59610773+Nikhilsain@users.noreply.github.com>
2021-05-24 00:12:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express"),
     app = express();
     
+app.use(function (req, res, next) {
+    
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+      
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+      
+      //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      
+        // Pass to next layer of middleware
+        next();
+     });
+
 
 
 const { json } = require("express");
